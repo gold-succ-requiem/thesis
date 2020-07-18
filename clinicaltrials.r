a6bad7d0f1e78e031b910027e93f9804a912c583
gold-succ-requiem <56791062+gold-succ-requiem@users.noreply.github.com>
2020-07-18 11:17:24 +1000

    Add files via upload

---
+++ clinicaltrials.r
@@ -0,0 +1,32 @@
+# obtains data from ClinicalTrials.gov, hopefully including ADMET and ADE data
+# input: ClinicalTrials.gov query
+# output: CSV of study details
+
+#=========
+
+# load packages
+library(dplyr)
+library(jsonlite)
+library(RCurl)
+
+#=========
+
+# FUNCTION: obtain JSON
+## obtain study records: phase 3+, completed
+## in JSON format: to circumvent security issues from https; and because CSV is not supported
+fetch.json <- function(url) {
+    getURL(url) %>%
+        fromJSON()
+}
+
+#=========
+
+# Pass URL to functions
+## convert JSON to dataframe via as.data.frame()
+## convert and write to CSV via write.csv()
+u <- "https://clinicaltrials.gov/api/query/full_studies?expr=&recrs=e&recrs=l&phase=2&phase=3&min_rnk=1&max_rnk=100&fmt=JSON"
+fetch.json(u) %>%
+    as.matrix(as.data.frame()) %>%
+    write.csv(.,"clinicaltrials.csv")
+
+#=========
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Drug Repositioning
+
+# Description
+## Background
+A drug repositioning system that incorporates a diversity of chemical, pharmacological, and biological data. A key aspect of the project is to incorporate side effect and toxicity data; it is intended that dissimilarities in side effect and toxicity between drugs, rather than similarities, will be used to recommend similar drugs.
+
+## Features
+- data collection and processing
+- feature selection and dimensionality reduction
