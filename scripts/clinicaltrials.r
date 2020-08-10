d4c7d849cb4c33ff275579766357fa7f3b7eaf4d
prvaldez <56791062+prvaldez@users.noreply.github.com>
2020-08-10 17:31:35 +1000

    Add files via upload

--- scripts/clinicaltrials.r
+++ scripts/clinicaltrials.r
@@ -1,33 +1,20 @@
-# obtains data from ClinicalTrials.gov, hopefully including ADMET and ADE data
-# input: ClinicalTrials.gov query
-# output: CSV of study details
-
-#=========
-
-# load packages
+# yo angelo!
 library(dplyr)
-library(jsonlite)
-library(RCurl)
+library(rclinicaltrials)
 
-#=========
+# Sanity check
+# clinicaltrials_count(query = c("phase=2", "phase=3", "type=Intr", "rslt=With"))
 
-# FUNCTION: obtain JSON
-## obtain study records: phase 3+, completed
-## in JSON format: to circumvent security issues from https; and because CSV is not supported
-fetch.json <- function(url) {
-    getURL(url) %>%
-        fromJSON()
-}
+# Downloads list of lists of data
+dl <- clinicaltrials_download(query = c("phase=2", "phase=3", "type=Intr", "rslt=With"), count = NULL, include_results = T)
 
-#=========
+# Merges arm and intervention dataframes into csv
+dl$study_information %>% 
+    c('arms', 'interventions') %>%
+    merge(.,., by = "nct_id") %>%
+    write.csv(., file = "clinicaltrials.csv")
 
-# Pass URL to functions
-## convert JSON to dataframe via as.data.frame()
-## convert and write to CSV via write.csv()
-## NOTE: will write to directory "../data"
-u <- "https://clinicaltrials.gov/api/query/full_studies?expr=&recrs=e&recrs=l&phase=2&phase=3&min_rnk=1&max_rnk=100&fmt=JSON"
-fetch.json(u) %>%
-    as.matrix(as.data.frame()) %>%
-    write.csv(.,"../data/clinicaltrials.csv")
-
-#=========
+# To-do list
+# - filter rows by including "intervention_type == 'Drug'" only
+# - edit two columns from study info df
+# - run through katana
