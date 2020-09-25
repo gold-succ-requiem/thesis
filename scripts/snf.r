074d4719ff7736081c185e6438b8b8679e35d925
prvaldez <56791062+prvaldez@users.noreply.github.com>
2020-09-25 15:00:28 +1000

    Update snf.r

--- scripts/snf.r
+++ scripts/snf.r
@@ -2,7 +2,8 @@
 ## Load packages
 library(data.table)
 library(dplyr)
-library(grDevices)
+library(ggplot2)
+library(reshape2)
 library(SNFtool)
 
 # Array of RepoDB unique drug IDs
@@ -10,6 +11,7 @@ repodb.ids <- fread("../data/repoDB_full.csv") %>%
     select(drug_id) %>%
     distinct(., drug_id, .keep_all = T)
 
+# PREPROCESSING
 # Chemical structure
 ## Load structure sims as matrix
 struct.sim <- fread("../data/chem_similarity.csv") %>%
@@ -24,19 +26,33 @@ struct.sim.repodb <- struct.sim[ ,which(colnames(struct.sim) %in% repodb.ids$dru
 struct.sim.repodb <- subset(struct.sim.repodb, rownames(struct.sim.repodb) %in% repodb.ids$drug_id)
 
 ## Make elements numeric -- so `affinityMatrix()` won't throw a hissy fit
-struct.sim.repodb <- `dimnames<-`(`dim<-`(as.numeric(struct.sim.repodb), dim(struct.sim.repodb)), dimnames(struct.sim.repodb))
+struct.sim.repodb <- apply(struct.sim.repodb, 2, as.numeric) %>%
+    `rownames<-`(., colnames(.))
+
+## Remove NA
+struct.sim.repodb <- struct.sim.repodb[rowSums(is.na(struct.sim.repodb)) != ncol(struct.sim.repodb), ]
+struct.sim.repodb <- struct.sim.repodb[ ,colSums(is.na(struct.sim.repodb)) < nrow(struct.sim.repodb)]
 
 # Target structure
 ## Load target similarities as matrix
-seq.sim <- fread("../data/protein_similarity.csv") %>%
+seq.sim <- fread("../data/target_similarity.csv") %>%
     as.matrix()
 
-## Divide matrix by 100 to make everything =< 1
-seq.sim <- seq.sim * (1/100)
+## Convert first column to row names
+rownames(seq.sim) <- seq.sim[,1]
+seq.sim <- seq.sim[,-1]
 
 ## Subset based on RepoDB
 seq.sim.repodb <- seq.sim[ ,which(colnames(seq.sim) %in% repodb.ids$drug_id)]
 seq.sim.repodb <- subset(seq.sim.repodb, rownames(seq.sim.repodb) %in% repodb.ids$drug_id)
+
+## Make elements numeric -- so `affinityMatrix()` won't throw a hissy fit
+seq.sim.repodb <- apply(seq.sim.repodb, 2, as.numeric) %>%
+    `rownames<-`(., colnames(.))
+
+## Remove NA
+seq.sim.repodb <- seq.sim.repodb[rowSums(is.na(seq.sim.repodb)) != ncol(seq.sim.repodb), ]
+seq.sim.repodb <- seq.sim.repodb[ ,colSums(is.na(seq.sim.repodb)) < nrow(seq.sim.repodb)]
 
 # Pathway similarity
 path.sim <- fread("../data/path_similarity_v2.csv") %>%
@@ -51,7 +67,12 @@ path.sim.repodb <- path.sim[ ,which(colnames(path.sim) %in% repodb.ids$drug_id)]
 path.sim.repodb <- subset(path.sim.repodb, rownames(path.sim.repodb) %in% repodb.ids$drug_id)
 
 ## Make elements numeric -- so `affinityMatrix()` won't throw a hissy fit
-#path.sim.100 <- apply(path.sim.100, 2, as.numeric)
+path.sim.repodb <- apply(path.sim.repodb, 2, as.numeric) %>%
+    `rownames<-`(., colnames(.))
+
+## Remove NA
+path.sim.repodb <- path.sim.repodb[rowSums(is.na(path.sim.repodb)) != ncol(path.sim.repodb), ]
+path.sim.repodb <- path.sim.repodb[ ,colSums(is.na(path.sim.repodb)) < nrow(path.sim.repodb)]
 
 # GO BP similarity
 bp.sim <- fread("../data/GO_Sim_BP_combined.csv") %>%
@@ -66,7 +87,13 @@ bp.sim.repodb <- bp.sim[ ,which(colnames(bp.sim) %in% repodb.ids$drug_id)]
 bp.sim.repodb <- subset(bp.sim.repodb, rownames(bp.sim.repodb) %in% repodb.ids$drug_id)
 
 ## Make elements numeric -- so `affinityMatrix()` won't throw a hissy fit
-bp.sim.repodb <- `dimnames<-`(`dim<-`(as.numeric(bp.sim.repodb), dim(bp.sim.repodb)), dimnames(bp.sim.repodb))
+bp.sim.repodb <- apply(bp.sim.repodb, 2, as.numeric) %>%
+    `rownames<-`(., colnames(.))
+
+## Remove NA
+bp.sim.repodb <- bp.sim.repodb[rowSums(is.na(bp.sim.repodb)) != ncol(bp.sim.repodb), ]
+bp.sim.repodb <- bp.sim.repodb[ ,colSums(is.na(bp.sim.repodb)) < nrow(bp.sim.repodb)]
+
 
 # GO CC similarity
 cc.sim <- fread("../data/GO_Sim_CC_combined.csv") %>%
@@ -76,15 +103,17 @@ cc.sim <- fread("../data/GO_Sim_CC_combined.csv") %>%
 rownames(cc.sim) <- cc.sim[,1]
 cc.sim <- cc.sim[,-1]
 
-## Omit rows all containing NA AND cols all containing NA, not actually needed
-cc.sim <- cc.sim[rowSums(is.na(cc.sim)) != ncol(cc.sim), ]
-cc.sim <- cc.sim[ ,colSums(is.na(cc.sim)) < nrow(cc.sim)]
-
-## Subset first 100 rows and cols
-cc.sim.100 <- cc.sim[1:100, 1:100]
+## Subset based on RepoDB
+cc.sim.repodb <- cc.sim[ ,which(colnames(cc.sim) %in% repodb.ids$drug_id)]
+cc.sim.repodb <- subset(cc.sim.repodb, rownames(cc.sim.repodb) %in% repodb.ids$drug_id)
 
 ## Make elements numeric -- so `affinityMatrix()` won't throw a hissy fit
-cc.sim.100 <- apply(cc.sim.100, 2, as.numeric)
+cc.sim.repodb <- apply(cc.sim.repodb, 2, as.numeric) %>%
+    `rownames<-`(., colnames(.))
+
+## Remove NA
+cc.sim.repodb <- cc.sim.repodb[rowSums(is.na(cc.sim.repodb)) != ncol(cc.sim.repodb), ]
+cc.sim.repodb <- cc.sim.repodb[ ,colSums(is.na(cc.sim.repodb)) < nrow(cc.sim.repodb)]
 
 # GO MF similarity
 mf.sim <- fread("../data/GO_Sim_MF_combined.csv") %>%
@@ -94,15 +123,26 @@ mf.sim <- fread("../data/GO_Sim_MF_combined.csv") %>%
 rownames(mf.sim) <- mf.sim[,1]
 mf.sim <- mf.sim[,-1]
 
-## Omit rows all containing NA AND cols all containing NA, not actually needed
-mf.sim <- mf.sim[rowSums(is.na(mf.sim)) != ncol(mf.sim), ]
-mf.sim <- mf.sim[ ,colSums(is.na(mf.sim)) < nrow(mf.sim)]
-
-## Subset first 100 rows and cols
-mf.sim.100 <- mf.sim[1:100, 1:100]
+## Subset based on RepoDB
+mf.sim.repodb <- mf.sim[ ,which(colnames(mf.sim) %in% repodb.ids$drug_id)]
+mf.sim.repodb <- subset(mf.sim.repodb, rownames(mf.sim.repodb) %in% repodb.ids$drug_id)
 
 ## Make elements numeric -- so `affinityMatrix()` won't throw a hissy fit
-mf.sim.100 <- apply(mf.sim.100, 2, as.numeric)
+mf.sim.repodb <- apply(mf.sim.repodb, 2, as.numeric) %>%
+    `rownames<-`(., colnames(.))
+
+## Remove NA
+mf.sim.repodb <- mf.sim.repodb[rowSums(is.na(mf.sim.repodb)) != ncol(mf.sim.repodb), ]
+mf.sim.repodb <- mf.sim.repodb[ ,colSums(is.na(mf.sim.repodb)) < nrow(mf.sim.repodb)]
+
+# COMMON DRUG ID COLLECTION
+## List of processed similarity matrices
+mats.repodb <- list(struct.sim.repodb, seq.sim.repodb, path.sim.repodb, bp.sim.repodb, cc.sim.repodb, mf.sim.repodb)
+
+## Collect row names
+common.rows <- Reduce(intersect, lapply(mats.repodb, row.names))
+red.repodb <- lapply(mats.repodb, function(x) { x[row.names(x) %in% common.rows,] }) %>%
+    lapply(., function(x) {x[ ,which(colnames(x) %in% common.rows)]})
 
 # Similarity network fusion
 ## Set parameters
@@ -111,40 +151,33 @@ alpha <- 0.5 # hyperparameter
 t <- 20 # t iterations
 
 ## Construct similarity graphs
-W.struct <- affinityMatrix(struct.sim.repodb, K, alpha)
-W.bp <- affinityMatrix(bp.sim.repodb, K, alpha)
+W.all <- lapply(red.repodb, affinityMatrix, K, alpha)
 
 ## Fuse similarity graphs
-W <- list(W.struct, W.seq, W.path, W.bp, W.cc, W.mf) %>%
-    SNF(., K, t)
+W <- SNF(W.all, K, t)
 
 ## Output as sanity check
 fwrite(W, file = "../data/fused_similarity.csv")
 
-# Define parameters for clustering functions
-C <- 2
-nn <- 5
+# PERFORMANCE EVALUATION
+# Visualise fused matrix with heatmap
+## Convert matrix to table and normalise
+W.melt <- melt(W)
+W.melt$value <- (W.melt$value - min(W.melt$value)) / (max(W.melt$value) - min(W.melt$value))
 
-# Labels from `SNFtool`
-labels.snftool <- spectralClustering(W, C)
+# Load RepoDB based on common rows
+repodb <- fread("../data/repoDB_drugPair_Transformation.csv") %>%
+    `[`(, c(2, 1, 3)) %>%
+    left_join(., W.melt, by = c("DrugBank.ID1" = "Var2", "DrugBank.ID2" = "Var1"))
 
-## Display clusters
-png("../data/snf_clusters.png")
-displayClusters(W, labels.snftool)
-dev.off()
+W.melt.adj <- W.melt %>%
+    filter(Var2 %in% repodb$DrugBank.ID1) %>%
+    filter(Var1 %in% repodb$DrugBank.ID2)
 
-# Spectral clustering and visualise eigenvalues using `kknn`
-labels.kknn <- specClust(W, centers = C, nn = nn)
+## Visualisation
+ggplot(W.melt, aes(x = Var1, y = Var2, fill = value)) + 
+    geom_tile() + 
+    ggsave("../data/snf_heatmap.png")
 
-## Visualise eigenvalues
-labels.kknn$centers %>%
-    plot()
+# ROC
 
-# Spectral clustering, PCA, and visualisation using `Spectrum`
-#labels.spectrum <- Spectrum(W, showpca = T, fontsize = 8, dotsize = 2)
-labels.spectrum <- Spectrum(list(W.struct, W.seq, W.path, W.bp, W.cc, W.mf), showpca = T, fontsize = 8, dotsize = 2)
-pca(labels.spectrum$similarity_matrix, labels = labels.spectrum$assignments)
-
-# Label comparison for quality assurance
-labels.compare <- cbind(labels.snftool, labels.kknn$cluster)
-labels.acc <- sum(labels.compare[,1] == labels.compare[,2]) / dim(labels.compare)[1]
