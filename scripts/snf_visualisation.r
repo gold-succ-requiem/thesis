ee18feeafac100bae2914e53d0af696450da4b6e
prvaldez <56791062+prvaldez@users.noreply.github.com>
2020-10-06 23:38:09 +1100

    Update snf_visualisation.r

--- scripts/snf_visualisation.r
+++ scripts/snf_visualisation.r
@@ -8,13 +8,13 @@ library(gplots)
 snf <- fread("../data/new_net_info_V7_pval.csv") %>%
     column_to_rownames(var = "rn") %>%
     as.matrix()
-snf[upper.tri(snf, diag = T)] <- 0
-snf <- data.table(
-    row = rep(seq_len(nrow(snf)), ncol(snf)), 
-    col = rep(seq_len(ncol(snf)), each = nrow(snf)), 
-    value = c(snf)
-    ) %>%
-    subset(., !(value == 0))
+snf[upper.tri(snf, diag = T)] <- NA
+#snf <- data.table(
+#    row = rep(seq_len(nrow(snf)), ncol(snf)), 
+#    col = rep(seq_len(ncol(snf)), each = nrow(snf)), 
+#    value = c(snf)
+#    ) %>%
+#    subset(., !(value == 0))
 
 # Load RepoDB based on common rows
 repodb <- fread("../data/repoDB_drugPair_Transformation.csv") %>%
@@ -44,9 +44,18 @@ snf.melt.adj <- snf.melt %>%
 #    ggsave("../data/struct_aff_heatmap.png")
 
 ## Visualisation -- Fused network
-ggplot(snf.melt, aes(x = Var1, y = Var2, fill = value)) + 
-    geom_tile() + 
-    theme(axis.text.x = element_blank(), axis.text.y = element_blank()) +
-    labs(x = "Drug ID", y = "Drug ID") +
-    guides(fill = guide_legend(title = "Similarity")) + 
-    ggsave("../data/snf_heatmap.png")
+heatmap.2(
+    snf, 
+    dendrogram = "none",
+    symm = T,
+    #na.color = par("bg"),
+    scale="none", 
+    Rowv=NA, 
+    #Colv=NA, 
+    #margins(5,5),
+    #cexRow=0.5,cexCol=1.0,
+    key=TRUE,keysize=1.5, 
+    trace="none",
+    labRow = NULL,
+    labCol = NULL
+    )
