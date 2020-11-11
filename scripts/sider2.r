b1b47559aa1136be50aa4550e3110bdfa792f0a3
prvaldez <56791062+prvaldez@users.noreply.github.com>
2020-11-11 15:43:23 +1100

    Update sider2.r

--- scripts/sider2.r
+++ scripts/sider2.r
@@ -20,11 +20,11 @@ meddra.sub <- meddra %>%
     select(stitch.id.stereo, umls.concept.id.meddra, meddra.concept, is.placebo) %>%
     add_column(freq = rowMeans(select(meddra, starts_with("freq.")))) %>%
     subset(meddra.concept == "PT" & is.placebo == "") %>%
-    select(-one_of(c('meddra.concept', 'is.placebo'))) %>%
+    select(-one_of(c("meddra.concept", "is.placebo"))) %>%
     pivot_wider(names_from = umls.concept.id.meddra, values_from = freq, values_fn = median) %>%
-    merge(., drugbank, by.x = "stitch.id.stereo", by.y = "STITCH_ID", all = T) %T>%
+    as_tibble() %>%
+    full_join(., drugbank, by = c("stitch.id.stereo" = "STITCH_ID")) %>%
+    relocate(., `DrugBank ID`, .after = stitch.id.stereo) %>%
+    drop_na(., `DrugBank ID`) %>%
+    select(., -stitch.id.stereo) %T>%
     saveRDS(., file = "meddra.sub.rds")
-## How to add column of DrugBank IDs
-## mapping to drugbank data frame
-
-# Match stereo STITCH IDs to CIDs -- test here
